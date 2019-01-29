b93027c31756a9f27df3e8ed52764b2868e3e80c
MercurialMune <munenecyp@gmail.com>
2019-01-29 16:32:29 +0300

    initial commit add new files to Password-Locker project

---
+++ user.py
@@ -0,0 +1,19 @@
+class User:
+    '''
+    a class that generates new instances of users
+    '''
+    pass
+
+    users_array = []
+
+    def __init__(self, first_name, last_name, phone_number, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.email = email
+
+    def save_user_details(self):
+        '''
+        save_contact method saves contact objects into contact_list
+        '''
+        User.users_array.append(self)
